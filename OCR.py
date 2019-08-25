19fb9c445a2bafeee471195f6ee48886bd8817e9
Szabó Tamás <szabotomika97@gmail.com>
2019-08-25 13:42:32 +0200

    some unnecessary comments deleted, attempts to improve recognition

--- OCR.py
+++ OCR.py
@@ -5,39 +5,6 @@ import pytesseract
 from PIL import Image
 from imutils import rotate
 
-"""
-cap = cv2.VideoCapture(0)
-
-if cap.isOpened():
-    ret, frame = cap.read()
-else:
-    ret = False
-
-while ret:
-    ret, frame = cap.read()
-
-    grey = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    edges = cv2.Canny(grey,50,250,apertureSize=5, L2gradient=True)
-í
-    lines = cv2.HoughLines(edges,1, np.pi/180,200)
-
-    if lines is not None:
-        for rho, theta, in lines[0]:
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a*rho
-            y0 = b*rho
-            pts1 = (int(x0 + 1000*(-b)),  int(y0 + 1000*(a)))
-            pts2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
-            cv2.line(frame,pts1,pts2,(0,255,0), 1)
-
-    cv2.imshow("Test", frame)
-
-    if cv2.waitKey(1) == 27:
-        break
-cv2.destroyAllWindows()
-cap.release()
-"""
 
 def get_rotation_angle(edges):
     left_is_smaller = True
@@ -87,26 +54,36 @@ def get_rotation_angle(edges):
     return angle
 
 
-
-img = cv2.imread("enar.png")
+img = cv2.imread("enar2.png")
 cv2.imshow("orig", img)
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray, 75, 150)
+
 #img = rotate(img, get_rotation_angle(edges))
+(h, w) = img.shape[:2]
+center = (w / 2, h / 2)
+scale = 1.0
+
+M = cv2.getRotationMatrix2D(center, get_rotation_angle(edges),scale)
+img = cv2.warpAffine(img, M, (w, h))
+
+
 h = img.shape[0]
-img= img[int(h/3):, :]
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-th3 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 5)  # thresholding
+img= img[int(h/4):, :]
+img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+kernel = np.ones((1, 1), np.uint8)
+img = cv2.dilate(img, kernel, iterations=1)
+img = cv2.erode(img, kernel, iterations=1)
+th3 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 5)  # thresholding
 th3 = cv2.resize(th3, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)  # interpolation for better results
-th3 = cv2.medianBlur(th3, 5)
-str= pytesseract.image_to_string(th3, config='-c tessedit_char_whitelist=0123456789 --oem 0 --psm 6')
-#ret, thresh = cv2.threshold(grey,127,255,cv2.THRESH_BINARY)
+cv2.imshow("before blur", th3)
+#th3 = cv2.medianBlur(th3, 3)
+th3 = cv2.bilateralFilter(th3, 9, 50, 150)
 
-edges = cv2.Canny(cv2.cvtColor(img,cv2.COLOR_BGR2GRAY),75,150)
-cv2.imshow("th3", th3)
-cv2.imshow("enar", img)
+str_ = pytesseract.image_to_string(th3, config='-c tessedit_char_whitelist=0123456789 --oem 0 --psm 6')
 
-print(str)
+
+print(str_)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 

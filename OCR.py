40dee32b1dbd5b2e07412b6d18a97fa2f4b9a676
Szabó Tamás <szabotomika97@gmail.com>
2019-09-26 18:34:02 +0200

    attempts

--- OCR.py
+++ OCR.py
@@ -54,7 +54,7 @@ def get_rotation_angle(edges):
     return angle
 
 
-img = cv2.imread("enar2.png")
+img = cv2.imread("images/enar2.png")
 cv2.imshow("orig", img)
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray, 75, 150)
@@ -65,24 +65,29 @@ center = (w / 2, h / 2)
 scale = 1.0
 
 M = cv2.getRotationMatrix2D(center, get_rotation_angle(edges),scale)
-img = cv2.warpAffine(img, M, (w, h))
+#img = cv2.warpAffine(img, M, (w, h))
 
 
 h = img.shape[0]
 img= img[int(h/4):, :]
 img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 kernel = np.ones((1, 1), np.uint8)
-img = cv2.dilate(img, kernel, iterations=1)
-img = cv2.erode(img, kernel, iterations=1)
+#img = cv2.dilate(img, kernel, iterations=1)
+#img = cv2.erode(img, kernel, iterations=1)
 th3 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 5)  # thresholding
-th3 = cv2.resize(th3, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)  # interpolation for better results
+(h, w) = th3.shape
+greater = h if h > w else w
+ratio = 150 / greater
+print("H: " + str(h) + ", W: " + str(w))
+if h < 150 and w < 150:
+    th3 = cv2.resize(th3, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_CUBIC)  # interpolation for better results
 cv2.imshow("before blur", th3)
 #th3 = cv2.medianBlur(th3, 3)
 th3 = cv2.bilateralFilter(th3, 9, 50, 150)
 
-str_ = pytesseract.image_to_string(th3, config='-c tessedit_char_whitelist=0123456789 --oem 0 --psm 6')
+str_ = pytesseract.image_to_string(th3, config='-c tessedit_char_whitelist=0123456789 --oem 1 --psm 6 --tessdata-dir /home/tom/PycharmProjects/opencv/')
 
-
+print("---")
 print(str_)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
